df633439cffa675a6d2112b668dba455c9f1f4d2
Joel <joel@crossref.org>
2021-09-23 22:22:45 -0500

    Fixed a number of bugs.

--- index.js
+++ index.js
@@ -12,16 +12,16 @@ function recalculate() {
   var startAge = age; 
   var death = parseInt(document.getElementById('death').value);
   
-  var value = new Array(death - age).fill(new Decimal(0));
+  var value = new Array(death - age + 1).fill(new Decimal(0));
 
   var ammortizationTable = [];
 
-  for (age; age < death; age++) {
+  for (age; age <= death; age++) {
     for (var x = 0; x < rules.length; x++) {
       if (age >= rules[x].childNodes[7].value && age <= rules[x].childNodes[9].value) {
         var currentAmount = value[age - startAge];
-        var changeAmount = new Decimal(rules[x].childNodes[3].value);
-        var changeFrequency = new Decimal(rules[x].childNodes[5].value);
+        var changeAmount = new Decimal(numbersOnly(rules[x].childNodes[3].value));
+        var changeFrequency = new Decimal(numbersOnly(rules[x].childNodes[5].value));
         var changeTotal = changeAmount.times(changeFrequency);
 
         if (rules[x].childNodes[1].value == "+") {
@@ -62,7 +62,7 @@ function add(event) {
   var div = document.createElement('div');
   div.className += 'data';
   div.className += ' flex-item';
-  div.innerHTML = 'I plan to <select id="operation" onchange="recalculate()" onmouseup="recalculate()"><option value="+">save</option><option value="-">spend</option></select> $<input id="amount" size="6" value="10000" onchange="recalculate()"></input> a <select id="frequency" onchange="recalculate()" onmouseup="recalculate()"><option value="1">year</option><option value="12">month</option></select> from age <input id="start" size="2" value="18" onchange="recalculate()"></input> to <input id="end" size="2" value="62" onchange="recalculate()"></input> <button onclick="remove(event); recalculate()">Delete</button>';
+  div.innerHTML = 'I plan to <select id="operation" onchange="recalculate()" onmouseup="recalculate()"><option value="+">save</option><option value="-">spend</option></select> <input id="amount" size="6" value="10000" onchange="formatMoney(event); recalculate()"></input> a <select id="frequency" onchange="recalculate()" onmouseup="recalculate()"><option value="1">year</option><option value="12">month</option></select> from age <input id="start" size="2" value="18" onchange="recalculate()"></input> to <input id="end" size="2" value="62" onchange="recalculate()"></input> <button onclick="remove(event); recalculate()">Delete</button>';
   event.srcElement.parentNode.parentNode.insertBefore(div, event.srcElement.parentNode);
 
   recalculate();
@@ -78,15 +78,15 @@ function numbersOnly(questionableNumber) {
   return questionableNumber.replace(/\D/g,'');
 }
 
-function formatStartingNetworth() {
-  var networth = numbersOnly(document.getElementById('startingNetworth').value);
-  document.getElementById('startingNetworth').value = formatter.format(networth);
+function formatMoney(event) {
+  event.srcElement.value = formatter.format(numbersOnly(event.srcElement.value));
 }
 
 function updateAmmortizationTable(data) {
-  for (var x=0; x < data.length; x++) {
-    var tbody = document.getElementById('ammortization');
+  var tbody = document.getElementById('ammortization');
+  tbody.innerHTML = '';
 
+  for (var x=0; x < data.length; x++) {
     var newRow = tbody.insertRow();
 
     columns = ['age', 'networth', 'contribution', 'interest'];
