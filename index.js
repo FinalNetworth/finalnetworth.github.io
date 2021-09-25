899b61471a511668a61ef25f409ed83d5e83b697
Joel <joel@crossref.org>
2021-09-24 20:26:56 -0500

    Remove unneeded events. Better defaults in the html. First round of saving data into the URL for sharing.

--- index.js
+++ index.js
@@ -5,13 +5,25 @@ var formatter = new Intl.NumberFormat(undefined, {
   maximumFractionDigits: 0,
 });
 
+window.onload = function() {
+  var data = JSON.parse(decodeURIComponent(window.location.hash.substr(1)));
+
+  for (var key in data) {
+    var element = document.getElementById(key);
+    element.value = data[key];
+    element.dispatchEvent(new Event('change'));
+  }
+
+  recalculate();
+}
+
 function recalculate() {
   var rules = document.querySelectorAll('.data');
 
   var age = parseInt(document.getElementById('age').value);
   var startAge = age; 
   var death = parseInt(document.getElementById('death').value);
-  
+
   var value = new Array(death - age + 1).fill(new Decimal(0));
 
   var ammortizationTable = [];
@@ -48,10 +60,18 @@ function recalculate() {
 
     compounded[x] = interest.times(networth).add(value[x]);
     ammortizationTable.push({'age': x + startAge, 'networth': formatMoney(networth.toNumber()), 'contribution': formatMoney(value[x].toNumber()), 'interest': formatMoney(compounded[x].sub(networth).sub(value[x]).toNumber())});
-  
   }
 
   document.getElementById('networth').innerHTML = formatter.format(parseInt(compounded[compounded.length - 1].toString()));
+
+  var saveData = {
+    'age': startAge, 
+    'death': death, 
+    'startingNetworth': numbersOnly(document.getElementById('startingNetworth').value),
+    'interest': numbersOnly(document.getElementById('interest').value)
+  };
+
+  window.history.pushState({}, '', '#' + encodeURIComponent(JSON.stringify(saveData)));
 
   updateAmmortizationTable(ammortizationTable);
 }
@@ -60,7 +80,7 @@ function add(event) {
   var div = document.createElement('div');
   div.className += 'data';
   div.className += ' flex-item';
-  div.innerHTML = 'I plan to <select id="operation" onchange="recalculate()" onmouseup="recalculate()"><option value="+">save</option><option value="-">spend</option></select> <input id="amount" size="6" value="10000" onchange="formatMoneyEvent(event); recalculate()"></input> a <select id="frequency" onchange="recalculate()" onmouseup="recalculate()"><option value="1">year</option><option value="12">month</option></select> from age <input id="start" size="2" value="18" onchange="recalculate()"></input> to <input id="end" size="2" value="62" onchange="recalculate()"></input> <button onclick="remove(event); recalculate()">Delete</button>';
+  div.innerHTML = 'I plan to <select id="operation" onchange="recalculate()"><option value="+">save</option><option value="-">spend</option></select> <input id="amount" size="6" value="10000" onchange="formatMoneyEvent(event); recalculate()"></input> a <select id="frequency" onchange="recalculate()"><option value="1">year</option><option value="12">month</option></select> from age <input id="start" size="2" value="18" onchange="recalculate()"></input> to <input id="end" size="2" value="62" onchange="recalculate()"></input> <button onclick="remove(event); recalculate()">Delete</button>';
   event.srcElement.parentNode.parentNode.insertBefore(div, event.srcElement.parentNode);
 
   recalculate();
